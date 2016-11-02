d4bf823202cd325c38a8239916e69cf718b8a902
zhangliu <zhangliuge@yeah.net>
2016-11-02 11:20:54 +0800

    add files

--- src/frontend/config.js
+++
@@ -1,5 +0,0 @@
-export default {
-  apiUrl: 'http://172.16.4.248/web/index.php',
-  localUrl: 'http://172.16.4.248/',
-  qiniuUrl: 'http://oertbv1rg.bkt.clouddn.com/',
-}
---
+++ src/frontend/config_bak.js
@@ -0,0 +1,5 @@
+export default {
+  apiUrl: 'http://127.0.0.1:8001/src/backend/web/index.php',
+  // localUrl: 'http://127.0.0.1:8001/src/backend/',
+  qiniuUrl: 'http://oertbv1rg.bkt.clouddn.com/',
+}
