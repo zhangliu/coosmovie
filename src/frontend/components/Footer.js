0f07944fb43f1e9cf5c56b577abd5238e21e552d
张云念 <zhangyunnian@innobuddy.com>
2016-11-15 20:23:23 +0800

    add files

--- src/frontend/components/Footer.js
+++ src/frontend/components/Footer.js
@@ -9,7 +9,12 @@ export default class className extends React.Component {
 
   render() {
     return (
-      <div className='footer'></div>
+      <div className='footer'>
+        <div className='footerContent'>
+          <span>developed by zhangliu, email: </span>
+          <a href='mailto:zhangliuge@yeah.net'>zhangliuge@yeah.net</a>
+        </div>
+      </div>
     )
   }
 }
